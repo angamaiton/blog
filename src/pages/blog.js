8d34849329509243874945c784ddad42aca2e720
Kjartan Angamaiton <ofurmadur@icloud.com>
2018-01-05 23:10:20 -0600

    Bugfix

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 import PostList from '../components/PostList';
 
-class Index extends Component {
+class Blog extends Component {
   render() {
     const { data } = this.props;
     const siteTitle = data.site.siteMetadata.title;
+    const posts = data.allMarkdownRemark.edges;
 
     return (
       <section>
-        <h2>Home</h2>
+        <h2>Blog</h2>
         <Helmet title={siteTitle} />
         <PostList posts={data.allMarkdownRemark.edges} />
       </section>
@@ -17,16 +18,19 @@ class Index extends Component {
   }
 }
 
-export default Index;
+export default Blog;
 
 export const pageQuery = graphql`
-  query IndexQuery {
+  query BlogQuery {
     site {
       siteMetadata {
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { fileAbsolutePath: { regex: "/(/src/pages)/.*\\.md$/" } }
+    ) {
       edges {
         node {
           excerpt
